b3c07741a4ed83b42ba8a5c1b71e26165b6ed968
Marina-Brovinschii <141269694+Marina-Brovinschii@users.noreply.github.com>
2023-12-05 15:04:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function reply() {
+    personName = nameInput.value
+    message ="Hello " + prompt("Nume", personName = nameInput.value) + "!"
+    msgContainer.innerHTML = message
+}
+let name = prompt("Nume", personName = nameInput.value);
+alert("Hello " + name + " !");
